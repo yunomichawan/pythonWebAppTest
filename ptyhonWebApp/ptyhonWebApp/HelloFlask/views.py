0d862d913c58d844f75190964ed26a1ba5422ad7
yunomichawan <ddlkgjbdnm@gmail.com>
2022-07-02 17:16:32 +0900

    「2:Flaskアプリを作成する」 完了

--- ptyhonWebApp/ptyhonWebApp/HelloFlask/views.py
+++ ptyhonWebApp/ptyhonWebApp/HelloFlask/views.py
@@ -1,8 +1,17 @@
-
-from flask import Flask
+from datetime import datetime
+from flask import render_template
 from HelloFlask import app
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    # Flask は自動的に templates フォルダーでテンプレートを探す。
+    # フォルダ名は固定
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
